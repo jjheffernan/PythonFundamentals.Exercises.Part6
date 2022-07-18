2317ddb19ddbedf31a7fc8d294b5eae4f58d7fd1
jjheffernan <jjheffernan99@gmail.com>
2022-07-18 00:15:49 -0400

    added kelvin tests.

---
+++ test_temperature_utils_v2.py
@@ -0,0 +1,58 @@
+import unittest
+import temperature_utils_v2
+
+
+class TemperatureUtilsTest(unittest.TestCase):
+
+    def test_convert_to_celsius(self):
+        test_cases = [
+            (32, 0),
+            (68, 20),
+            (100, 37.78),
+            (104, 40)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_v2.convert_to_celsius(temp_in))
+
+    def test_convert_to_fahrenheit(self):
+        test_cases = [
+            (-17.7778, 0),
+            (0, 32),
+            (100, 212)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_v2.convert_to_fahrenheit(temp_in))
+
+    def test_convert_to_kelvin(self):
+        test_cases = [
+            (-17.7778, 0),
+            (0, 32),
+            (100, 212)
+        ] # need to update numbers
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_v2.convert_to_kelvin(temp_in))
+
+    def test_temperature_tuple(self):
+        temps_input = (32, 68, 100, 104)
+        expected = ((32, 0.0), (68, 20.0), (100, 37.78), (104, 40.0))
+        actual = temperature_utils_v2.temperature_tuple(temps_input, "f")
+        self.assertEqual(expected, actual)
+
+    def test2_temperature_tuple(self):
+        temps_input = (-17.7778, 0, 100)
+        expected = ((-17.7778, 0.0), (0, 32.0), (100, 212.0))
+        actual = temperature_utils_v2.temperature_tuple(temps_input, "c")
+        self.assertEqual(expected, actual)
+
+    def test3_temperature_tuple(self):
+        temps_input = (-17.7778, 0, 100)
+        expected = ((-17.7778, 0.0), (0, 32.0), (100, 212.0)) # need to update numbers
+        actual = temperature_utils_v2.temperature_tuple(temps_input, "k")
+        self.assertEqual(expected, actual)
+
+    def test3_temperature_tuple(self):
+        temps_input = (1, 2, 3)
+        self.assertEqual(tuple(), temperature_utils_v2.temperature_tuple(temps_input, "a"))
